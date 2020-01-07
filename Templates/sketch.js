2fad3b8c1a8f072fb13aee290726592f07591130
Dennis-Talkington <59609876+Dennis-Talkington@users.noreply.github.com>
2020-01-07 07:18:59 -0600

    Add files via upload

---
+++ Templates/sketch.js
@@ -0,0 +1,20 @@
+// References found at p5js.org
+
+// Add your variables here
+
+
+
+// initialize your program here
+function setup() {
+    createCanvas(500, 500);
+    
+}
+
+
+
+
+// Essentially a repeat command
+function draw() {
+    background(50);
+
+}
